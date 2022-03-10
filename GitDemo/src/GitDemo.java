9829dd8844eae0a34232bfaf87a856d4d07057e9
ZhanSansui <1062189799@qq.com>
2022-03-10 11:15:16 +0800

    add

---
+++ GitDemo/src/GitDemo.java
@@ -0,0 +1,12 @@
+/**
+ * @author Wen
+ * @version 1.0
+ * @data 2022/3/9 10:49
+ * @copyright 老九学堂
+ * @remark TODO
+ */
+public class GitDemo {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
